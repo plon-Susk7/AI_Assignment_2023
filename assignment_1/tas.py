e3ff4789bc459d51684505bb3112e0c703541ba7
priyash7 <priyashshah4@gmail.com>
2023-09-16 22:29:32 +0530

    major change to code

--- assignment_1/tas.py
+++ assignment_1/tas.py
@@ -14,99 +14,65 @@ from knowledge_base import knowledge_base
     'feedback' : [],
     'Activites' : []
 '''
-
-def no_place(data):
-    if len(data) == 0:
-        print("Sorry, we cannot recommend any place for you.")
-        return True
     
-def printDetails(places):
+def printDetails(score):
+    for key, value in score.items():
+        print(key, value)
 
+def add_score(score_map, places, score):
     for place in places:
-        print("Place: ", place['Place'])
-        print("Country: ", place['Country'])
-        print("Continent: ", place['Continent'])
-        print("Language: ", place['Language'])
-        print("Currency: ", place['Currency'])
-        print("Visa: ", place['Visa'])
-        print("Season to visit: ", place['Season_to_visit'])
-        print("Expected budget: ", place['Expected_budget'])
-        print("Rating: ", place['Rating'])
-        print("Activities: ", place['Activities'])
-        print("Feedback: ", place['feedback'])
-        print("\n")
+        if place['Place'] in score_map:
+            score_map[place['Place']] += score
+        else:
+            score_map[place['Place']] = score
+
+def filter_places(places, question, answer, score_map, score):
+    filtered_places = []
+    for place in places:
+        print(place)
+        if place[question] == answer:
+            filtered_places.append(place)
+    add_score(score_map, filtered_places, score)
+    return filtered_places
 
 def recommend_destination():
+    # creating a dictionary to add points for each question and places
+    score_map = {}
+    places = knowledge_base
 
-    visa = input("Do you need a visa? ")
-    # filtering places based on visa criteria first
-    if visa == "Yes":
-        places = list(filter(lambda x: x['Visa'] == "Yes", knowledge_base))
-    no_place(places)
-    printDetails(places)
-    
-    # filtering places based on expected budget
-    expected_budget = input("Enter your expected budget: ")
-    for place in places:
-        if place['Expected_budget'] != expected_budget:
-            places.remove(place)
-    
-    no_place(places)
-    printDetails(places)
-    
-    # filtering places based on season to visit
-    season_to_visit = input("Enter the season you want to visit: ")
-    for place in places:
-        if season_to_visit not in place['Season_to_visit']:
-            places.remove(place)
-            
-    no_place(places)
-    printDetails(places)
-    
-    # filtering places based on continent
-    continent = input("Enter the continent you want to visit: ")
-    for place in places:
-        if place['Continent'] != continent:
-            places.remove(place)
+    questions = [
+        ("Do you need a visa? ", "Visa", "Yes", 1),
+        ("Enter your expected budget: ", "Expected_budget", None, 1),
+        ("Enter the season you want to visit: ", "Season_to_visit", None, 1),
+        ("Enter the continent you want to visit: ", "Continent", None, 1),
+        ("Enter the language you want to use: ", "Language", None, 1),
+        ("Enter the activities you want to do (space-separated activities): ", "Activities", None, 1),
+        ("Enter the minimum rating you want: ", "Rating", None, 1)
+    ]
 
-    no_place(places)
-    printDetails(places)
-    
-    # filtering places based on language
-    language = input("Enter the language you want to use: ")
-    for place in places:
-        if language not in place['Language']:
-            places.remove(place)
-
-    no_place(places)
-    printDetails(places)
-    
-    # filtering places based on activities
-    activites = input("Enter the activities you want to do( space separated activities ): ").split(' ')
-    for place in places:
-        if activites not in place['Activities']:
-            places.remove(place)
-    
-    no_place(places)
-    printDetails(places)
-    
-    # filtering places based on rating
-    rating = input("Enter the minimum rating you want: ")
-    for place in places:
-        if place['Rating'] < rating:
-            places.remove(place)
-
-    no_place(places)
-    printDetails(places)
-    
-    # if we reach here, we have at least one place to recommend
-    print("We recommend the following places for you:")
-    for place in places:
-        print(place['Place'])
+    for question, filter_key, answer, score in questions:
+        user_input = input(question)
+        if filter_key == "Expected_budget":
+            places = filter_places(places, filter_key, user_input, score_map, score)
+        elif filter_key == "Season_to_visit":
+            filtered_places = []
+            for place in places:
+                if user_input in place[filter_key]:
+                    filtered_places.append(place)
+            add_score(score_map, filtered_places, score)
+            places = filtered_places
+        elif filter_key == "Rating":
+            places = filter_places(places, filter_key, float(user_input), score_map, score)
+        else:
+            places = filter_places(places, filter_key, user_input, score_map, score)
+        
+        printDetails(score_map)
 
 
-
-
+        if places:
+            print("We recommend the following places for you:")
+            for place in places:
+                print(place['Place'])
 
 
 def add_new_destination():
